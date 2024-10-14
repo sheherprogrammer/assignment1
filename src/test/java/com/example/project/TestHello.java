1b361a66457144e1b1c4a742d7e3d6b0bda1cfac
sheherprogrammer <149023859+sheherprogrammer@users.noreply.github.com>
2024-10-13 20:51:38 -0500

    Update TestHello.java

--- src/test/java/com/example/project/TestHello.java
+++ src/test/java/com/example/project/TestHello.java
@@ -1,6 +1,8 @@
 package com.example.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,22 +11,62 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.*;
 
+//import com.example.project.ProgramLauncher;
+
+
 public class TestHello {
 
-   @Test
-   public void testHelloWorld()
-   {
-     PrintStream originalOut = System.out;
-     ByteArrayOutputStream bos = new ByteArrayOutputStream();
-     System.setOut(new PrintStream(bos));
+	@Test
+	public void TestFib() {
+		
+		assertEquals(0, ProgramLauncher.computeFibonacciNumber(1));
+		assertEquals(1, ProgramLauncher.computeFibonacciNumber(2));
+		
+		assertEquals(34, ProgramLauncher.computeFibonacciNumber(10));
+		
+	}
+	
+	@Test
+	public void TestMedian() {
+		
+		int[] t1 = {1, 1, 2};
+		assertEquals(1, ProgramLauncher.findMedianOfThree(t1));
+		
+		int[] t2 = {2, 1, 2};
+		assertEquals(2, ProgramLauncher.findMedianOfThree(t2));
+		
+		int[] t3 = {2, 2, 1};
+		assertEquals(2, ProgramLauncher.findMedianOfThree(t3));
+		
+		int[] t4 = {3, 3, 3};
+		assertEquals(3, ProgramLauncher.findMedianOfThree(t4));
+		
+		int[] t5 = {4, 2, 1};
+		assertEquals(2, ProgramLauncher.findMedianOfThree(t5));
+		
+		int[] t6 = {4, 1, 2};
+		assertEquals(2, ProgramLauncher.findMedianOfThree(t6));
 
-     // action
-     Hello.main(null);
-
-     // assertion
-     assertEquals("Hello world!\n", bos.toString());
-
-     // undo the binding in System
-     System.setOut(originalOut);
-   }
+		
+	}
+	
+	@Test
+	public void TestValidMultChoice() {
+		
+		assertTrue(ProgramLauncher.validMultipleChoice('a'));
+		assertTrue(ProgramLauncher.validMultipleChoice('b'));
+		assertTrue(ProgramLauncher.validMultipleChoice('c'));
+		assertTrue(ProgramLauncher.validMultipleChoice('d'));
+		
+		assertTrue(ProgramLauncher.validMultipleChoice('A'));
+		assertTrue(ProgramLauncher.validMultipleChoice('B'));
+		assertTrue(ProgramLauncher.validMultipleChoice('C'));
+		assertTrue(ProgramLauncher.validMultipleChoice('D'));
+		
+		assertFalse(ProgramLauncher.validMultipleChoice('.'));
+		assertFalse(ProgramLauncher.validMultipleChoice('q'));
+		assertFalse(ProgramLauncher.validMultipleChoice('T'));
+		
+		
+	}
 }
